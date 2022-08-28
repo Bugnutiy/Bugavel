90e14c5cddff9f5ff3a5c2c0f22d485eba34e019
Bugnutiy <bigbag.minecrafter@gmail.com>
2022-08-28 21:00:48 +0300

    dbfix

--- application/config/db.php
+++ application/config/db.php
@@ -1,8 +1,8 @@
 <?php
 // dd($_SERVER);
 return [
-	'host' => $_SERVER['SERVER_ADDR'],
-	'name' => 'leosnew',
-	'user' => 'LeosNew',
-	'password' => 'poilka170196',
+	'host' => 'h902111921.mysql',
+	'name' => 'h902111921_leosnew',
+	'user' => 'h902111921_leonw',
+	'password' => 'y2+tSZaE',
 ];
