16b52f3f67cdccd5de709b703c5ebe76dfbc926b
Bugnutiy <bigbag.minecrafter@gmail.com>
2022-08-28 22:55:19 +0300

    Fix

--- application/config/db.php
+++ application/config/db.php
@@ -1,9 +1,18 @@
 <?php
 // dd($_SERVER);
+// return [
+// 	'host' => $_SERVER['SERVER_ADDR'],
+// 	'name' => 'leosnew',
+// 	'user' => 'LeosNew',
+// 	'password' => 'poilka170196',
+// 	'charset' => 'utf8mb4', //unicode_ci
+// ];
+
+// dd($_SERVER);
 return [
-	'host' => 'localhost',
-	'name' => 'leosnew',
-	'user' => 'leosnew',
-	'password' => 'poilka170196',
+	'host' => 'h902111921.mysql',
+	'name' => 'h902111921_leosnew',
+	'user' => 'h902111921_leonw',
+	'password' => 'y2+tSZaE',
 	'charset' => 'utf8mb4',//unicode_ci
 ];
