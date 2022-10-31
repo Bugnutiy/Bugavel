fe418f157d2c66627d9060e2187f5cd95f2ce7ec
Bugnutiy <bigbag.minecrafter@gmail.com>
2022-10-31 12:27:52 +0300

    31.10 1

--- application/config/db.php
+++ application/config/db.php
@@ -1,11 +1,11 @@
 <?php
-return [
-	'host' => 'h902111921.mysql',
-	'name' => 'h902111921_leosnew',
-	'user' => 'h902111921_leonw',
-	'password' => 'y2+tSZaE',
-	'charset' => 'utf8mb4',//unicode_ci
-];
+// return [
+// 	'host' => 'h902111921.mysql',
+// 	'name' => 'h902111921_leosnew',
+// 	'user' => 'h902111921_leonw',
+// 	'password' => 'y2+tSZaE',
+// 	'charset' => 'utf8mb4',//unicode_ci
+// ];
 
 // return [
 // 	'host' => '127.0.0.1',
@@ -14,3 +14,12 @@ return [
 // 	'password' => 'poilka170196',
 // 	'charset' => 'utf8mb4',//unicode_ci
 // ];
+
+//Работа ПК
+return [
+	'host' => 'localhost',
+	'name' => 'h902111921_leosnew',
+	'user' => 'h902111921_leonw',
+	'password' => 'y2+tSZaE',
+	'charset' => 'utf8mb4',//unicode_ci
+];
