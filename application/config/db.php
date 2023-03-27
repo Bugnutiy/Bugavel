85e24a4c1c5e88156ef4dfb60acf1c2246f55dba
Bugnutiy <bigbag.minecrafter@gmail.com>
2023-03-27 07:40:53 +0300

    44

--- application/config/db.php
+++ application/config/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
 	'host' => 'h902111921.mysql',
-	'name' => 'h902111921_lovibaza',
+	'name' => 'h902111921_lovinew',
 	'user' => 'h902111921_u0',
 	'password' => 'yz:pnWU9',
 	'charset' => 'utf8mb4',//unicode_ci
